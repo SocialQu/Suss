21c77ac50222662c40ea3a6e5067b99816d36cb2
Santiago8888 <santiago.marti67@gmail.com>
2021-05-28 11:04:22 -0500

    Imported Footer.

---
+++ suss/src/components/Footer.tsx
@@ -0,0 +1,9 @@
+export const Footer = () => <footer className='footer' style={{height:80, padding:'40px 0px 0px 0px', backgroundColor:'transparent'}}>
+    <div className='content has-text-centered'>
+        <p style={{color:'white'}}>
+            <strong style={{color:'white'}}>Süß, </strong> 
+            a software by 
+            <a href='https://twitter.com/SocialQui' style={{color:'skyblue'}}> SocialQui</a>. 
+        </p>
+    </div>
+</footer>
