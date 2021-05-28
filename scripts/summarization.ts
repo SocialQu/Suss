9ca5f8574221ae0f266e316dcca4c8f001479547
Santiago8888 <santiago.marti67@gmail.com>
2021-05-28 02:52:02 -0500

    Get similarity of sentences with respect to center

--- scripts/summarization.ts
+++ scripts/summarization.ts
@@ -6,21 +6,32 @@ import '@tensorflow/tfjs-node'
 import { transcript } from './data'
 
 
-export const average = (vectors: number[][]) => [...Array(vectors[0].length)].map((_, idx) => 
+const findCenter = (vectors: number[][]) => [...Array(vectors[0].length)].map((_, idx) => 
     vectors.reduce((d,i)=> d + i[idx], 0)/vectors.length
 )
 
+
+const getSimilarity = (center:number[], embedding: number[]) => {
+    if (center.length !== embedding.length) return Infinity
+    const delta = center.reduce((d, i, idx) => d + Math.abs(i - embedding[idx]), 0)
+    return delta
+}
+
 const summarize = async() => {
+    // Embed Sentences
     const sentences = transcript.split('\n')
     const model = await load()
     const tensors = await model.embed(sentences)
     const embeddings = await tensors.array()
     // console.log(embeddings[0])
 
-    const center = average(embeddings)
-    console.log(center.filter((_, i) => i < 10))
+    // Get Center
+    const center = findCenter(embeddings)
+    // console.log(center.filter((_, i) => i < 10))
 
-    // Compute Distance
+    // Compute Similarity
+    const similarities = embeddings.map(e => getSimilarity(center, e))
+    console.log('similarities:', similarities)
 
     // Get 5 - 10% of sentences by distance.
 
