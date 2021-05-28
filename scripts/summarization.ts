b93228fabfb709f1cce1fd1694ac3a3fe27c591a
Santiago8888 <santiago.marti67@gmail.com>
2021-05-28 02:41:45 -0500

    Compute a vectors' center.

--- scripts/summarization.ts
+++ scripts/summarization.ts
@@ -6,15 +6,19 @@ import '@tensorflow/tfjs-node'
 import { transcript } from './data'
 
 
+export const average = (vectors: number[][]) => [...Array(vectors[0].length)].map((_, idx) => 
+    vectors.reduce((d,i)=> d + i[idx], 0)/vectors.length
+)
+
 const summarize = async() => {
     const sentences = transcript.split('\n')
     const model = await load()
     const tensors = await model.embed(sentences)
     const embeddings = await tensors.array()
+    // console.log(embeddings[0])
 
-    console.log(embeddings[0])
-
-    // Find average
+    const center = average(embeddings)
+    console.log(center.filter((_, i) => i < 10))
 
     // Compute Distance
 
