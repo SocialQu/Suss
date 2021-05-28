a15c98a233153e172e609601f73b6daf219986d1
Santiago8888 <santiago.marti67@gmail.com>
2021-05-28 16:00:38 -0500

    Transcript CTA.

--- suss/src/views/Summarization.tsx
+++ suss/src/views/Summarization.tsx
@@ -1,4 +1,6 @@
+import { ctaStyle } from '../views/Landing'
 import { CSSProperties } from "react"
+
 
 const summarizationStyle:CSSProperties = {
     margin:'auto',
@@ -7,9 +9,16 @@ const summarizationStyle:CSSProperties = {
     paddingBottom:'3em'
 }
 
-export const Summarization = () => <div className='container' style={summarizationStyle}>
-    <p className='title is-1 has-text-centered' style={{color:'white', marginBottom:'0'}}> 
-        Meeting's Transcript
-    </p>
+export const Summarization = () => {
+    const click =() => {}
 
-</div>
+    return <div className='container' style={summarizationStyle}>
+        <p className='title is-1 has-text-centered' style={{color:'white', marginBottom:'0'}}> 
+            Meeting's Transcript
+        </p>
+
+        <div className='control' style={{ textAlign:'center', marginTop:'1em' }}>
+            <button className='button is-info' style={ctaStyle} onClick={click}> Get Started Now! </button>
+        </div>
+    </div>
+}
