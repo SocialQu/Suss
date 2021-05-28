7c84c1846645ccab99382903127c33d3fa01285c
Santiago8888 <santiago.marti67@gmail.com>
2021-05-27 20:59:06 -0500

    Embed transcript sentences.

--- scripts/summarization.ts
+++ scripts/summarization.ts
@@ -1,5 +1,19 @@
 // npx ts-node summarization
 
+import { load } from '@tensorflow-models/universal-sentence-encoder'
+import '@tensorflow/tfjs-node'
+
 import { transcript } from './data'
 
-console.log(transcript.split('\n').length)
+
+const summarize = async() => {
+    const sentences = transcript.split('\n')
+    const model = await load()
+    const tensors = await model.embed(sentences)
+    const embeddings = await tensors.array()
+
+    console.log(embeddings[0])
+}
+
+
+summarize().catch(console.log)
