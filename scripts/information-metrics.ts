87cead8edd02e4ef7b26d7fc48b39b0bbffac115
Santiago8888 <santiago.marti67@gmail.com>
2021-05-31 13:20:30 -0500

    Compute information and rank sentences.

--- scripts/information-metrics.ts
+++ scripts/information-metrics.ts
@@ -1,4 +1,5 @@
 // npx ts-node information-metrics
+import { transcript } from './data'
 
 
 const getStandardDeviation = (vector:number[]) => {
@@ -13,3 +14,22 @@ const getDictionary = (words:string[]) => words.reduce((d, i) => ({...d, [i]: d[
 
 interface iGetInformation { words:string[], dictionary:iDictionary, tokens:number}
 const getInformation = ({words, dictionary, tokens}:iGetInformation) => words.reduce((d, w) => d-=Math.log(dictionary[w]/tokens), 0)
+
+
+const rankSentences = () => {
+    const words = transcript.split(/[\s\n]+/)
+    const tokens = transcript.split('\n')
+    const dictionary = getDictionary(words)
+
+    const sentences = tokens.map((text, i) => ({ 
+        text, 
+        order:i,
+        information:getInformation({ words:text.split(' '), dictionary, tokens:words.length })
+    }))
+
+    const sorted = sentences.sort(({information:a}, {information:b}) => a > b ? -1 : 1)
+    const filtered = sorted.filter((_, idx) => idx < 10)
+    console.log(filtered)
+}
+
+rankSentences()
