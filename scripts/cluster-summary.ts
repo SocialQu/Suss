52277d1a23d72d11fd8776958392e717f3470a42
Santiago8888 <santiago.marti67@gmail.com>
2021-05-31 11:38:33 -0500

    Assign clusters to sentences.

--- scripts/cluster-summary.ts
+++ scripts/cluster-summary.ts
@@ -7,15 +7,33 @@ import '@tensorflow/tfjs-node'
 
 const kmeans = require('ml-kmeans')
 
+export interface iSentences {
+    text:string
+    cluster:number
+    order:number
+    d1?:number[]
+    d2?:number[]
+    embeddings?:number[]
+}
+
+
 const summarize = async() => {
     // Embed Sentences
-    const sentences = transcript.split('\n')
+    const tokens = transcript.split('\n')
     const model = await load()
-    const tensors = await model.embed(sentences)
+    const tensors = await model.embed(tokens)
     const embeddings = tensors.arraySync()
 
     const { clusters } = kmeans(embeddings, 5)
-    console.log(clusters.length)
+    // console.log(clusters.length)
+
+    const sentences:iSentences[] = tokens.map((t,i) => ({
+        text:t, 
+        order:i,
+        cluster:clusters[i]
+    }))
+
+    console.log(sentences)
 }
 
 
