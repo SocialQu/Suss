e555d346080c74b7c7d2a3aa2d9ceed05b7c21d5
Santiago8888 <santiago.marti67@gmail.com>
2021-05-31 12:15:37 -0500

    Import data with clusters.

--- suss/src/views/Charts.tsx
+++ suss/src/views/Charts.tsx
@@ -1,23 +1,22 @@
 import { Scatter } from '../atoms/Scatter'
-import D1 from '../data/1D-data.json'
-import D2 from '../data/2D-data.json'
+import data from '../data/cluster-data.json'
 
 export const Charts = () => {
     return <div className='container' >
         <p 
             className='title is-1 has-text-centered' 
-            style={{color: 'white'}}
+            style={{color: 'white', marginBottom:'1em'}}
         > Meeting's Charts </p>
 
         <div>
             <Scatter 
                 label={'Information Through Time'}
-                data={[D1.map(([y], x) => ({x, y, name:''}))]} 
+                data={data.map(clusters => clusters.map(({ text, d1 }) => ({ name:text, x:d1[0], y:d1[1] }) ))}
             />
 
             <Scatter 
-                label={'2D Sentences Representation'}
-                data={[D2.map(([x,y]) => ({x, y, name:''}))]} 
+                label={'2D Sentence Representation'}
+                data={data.map(clusters => clusters.map(({ text, d2 }) => ({ name:text, x:d2[0], y:d2[1] }) ))}
             />
         </div>
     </div>
