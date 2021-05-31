248b3ce52076c2f0d7edc7a3b2d41ea1a4a03c0a
Santiago8888 <santiago.marti67@gmail.com>
2021-05-31 15:06:30 -0500

    Cluster stats.

--- scripts/information-metrics.ts
+++ scripts/information-metrics.ts
@@ -1,8 +1,8 @@
 // npx ts-node information-metrics
+import clusters from '../suss/src/data/cluster-data.json'
 import { transcript } from './data'
 
-
-const getStandardDeviation = (vector:number[]) => {
+const getVariance = (vector:number[]) => {
     const n = vector.length
     const mean = vector.reduce((a, b) => a + b, 0) / n
     const deviation = Math.sqrt(vector.map(x => Math.pow(x - mean, 2)).reduce((a, b) => a + b) / n)
@@ -15,21 +15,37 @@ const getDictionary = (words:string[]) => words.reduce((d, i) => ({...d, [i]: d[
 interface iGetInformation { words:string[], dictionary:iDictionary, tokens:number}
 const getInformation = ({words, dictionary, tokens}:iGetInformation) => words.reduce((d, w) => d-=Math.log(dictionary[w]/tokens), 0)
 
+const words = transcript.split(/[\s\n]+/)
+const sentences = transcript.split('\n')
+const dictionary = getDictionary(words)
 
 const rankSentences = () => {
-    const words = transcript.split(/[\s\n]+/)
-    const tokens = transcript.split('\n')
-    const dictionary = getDictionary(words)
 
-    const sentences = tokens.map((text, i) => ({ 
+    const tokens = sentences.map((text, i) => ({ 
         text, 
         order:i,
         information:getInformation({ words:text.split(' '), dictionary, tokens:words.length })
     }))
 
-    const sorted = sentences.sort(({information:a}, {information:b}) => a > b ? -1 : 1)
+    const sorted = tokens.sort(({information:a}, {information:b}) => a > b ? -1 : 1)
     const filtered = sorted.filter((_, idx) => idx < 10)
     console.log(filtered)
 }
 
-rankSentences()
+// rankSentences()
+
+const measureClusters = () => {
+    const sizes = clusters.map(c => c.length)
+    const averages = clusters.map(c => c.reduce((d, { order }) => d+=order, 0)/c.length)
+    const variance = clusters.map(c => getVariance(c.map(({ order }) => order)))
+    const information = clusters.map(cluster => cluster.reduce((d, { text }) => 
+        d+=getInformation({ words:text.split(' '), dictionary, tokens:words.length })
+    , 0)/cluster.length)
+
+    console.log('sizes', sizes)
+    console.log('averages', averages)
+    console.log('variance', variance)
+    console.log('information', information)
+}
+
+measureClusters()
