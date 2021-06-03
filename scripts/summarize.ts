8bebe79e3c953a843e3eb64050e8f00ddb92dc9e
Santiago8888 <santiago.marti67@gmail.com>
2021-06-03 11:09:40 -0500

    Get Titles helper function.

--- scripts/summarize.ts
+++ scripts/summarize.ts
@@ -1,3 +1,6 @@
+import { getSimilarity } from "./utils"
+
+
 interface iConclusion {
     beginning:string[]
     middle:string[]
@@ -11,8 +14,23 @@ interface iSummary {
     conclusions: iConclusion
 }
 
+interface iSentence{
+    text:string
+    order:number
+    embeddings:number[]
+}
 
-const getTitles = ():string[] => []
+
+const getTitles = (sentences:iSentence[], center:number[]):string[] => {
+    const similarities = sentences.map((s, i) => ({ ...s, similarity:getSimilarity(s.embeddings, center)}))
+    const sortedSentences = [...similarities].sort(({ similarity:a }, { similarity:b }) => a > b ? 1 : -1)
+    const topSentences = sortedSentences.filter((_, i) => i < 10)
+    
+    console.log(topSentences.map(({ order, similarity}) => console.log(order, similarity)))
+    const titles = topSentences.map(({ text }) => text)
+    return titles
+}
+
 const getTopics = ():string[][] => []
 const getNotes = ():string[] => []
 const getConclusion = ():iConclusion => ({
@@ -22,9 +40,14 @@ const getConclusion = ():iConclusion => ({
 })
 
 
-const summarize = (text:string):iSummary => ({
-    titles:getTitles(),
-    topics:getTopics(),
-    notes:getNotes(),
-    conclusions:getConclusion()
-})
+const summarize = (sentences:iSentence[]):iSummary => {
+
+    return {
+        titles:getTitles(sentences, []),
+        topics:getTopics(),
+        notes:getNotes(),
+        conclusions:getConclusion()
+    }
+}
+
+summarize([])
