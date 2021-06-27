59853bcd34fd4db109c6fe73054e393d0c432c2a
Santiago8888 <santiago.marti67@gmail.com>
2021-06-27 12:51:53 -0500

    Get topics, clusters interface and embed tokens.

--- scripts/summarize.ts
+++ scripts/summarize.ts
@@ -1,4 +1,5 @@
-import { getSimilarity } from "./utils"
+import { getSimilarity, getDictionary, getEmbeddings } from "./utils"
+const kmeans = require('ml-kmeans')
 
 
 interface iConclusion {
@@ -14,24 +15,54 @@ interface iSummary {
     conclusions: iConclusion
 }
 
-interface iSentence{
+interface iSentence {
     text:string
     order:number
+    cluster?:number
     embeddings:number[]
 }
 
 
-const getTitles = (sentences:iSentence[], center:number[]):string[] => {
+interface iCentroid {
+    centroid:number[]
+    error:number
+    size:number
+}
+
+interface iClustered {
+    clusters:number[]
+    centroids:iCentroid[]
+}
+
+
+const getTitles = (sentences:iSentence[]):string[] => {
+    const center:number[] = [] // TODO: Search for center.
     const similarities = sentences.map((s, i) => ({ ...s, similarity:getSimilarity(s.embeddings, center)}))
     const sortedSentences = [...similarities].sort(({ similarity:a }, { similarity:b }) => a > b ? 1 : -1)
     const topSentences = sortedSentences.filter((_, i) => i < 10)
-    
-    console.log(topSentences.map(({ order, similarity}) => console.log(order, similarity)))
+
     const titles = topSentences.map(({ text }) => text)
     return titles
 }
 
-const getTopics = ():string[][] => []
+const getTopics = (sentences:iSentence[]):string[][] => {
+    const embeddings = sentences.map(({embeddings}) => embeddings)
+    const { clusters, centroids }:iClustered = kmeans(embeddings, 5)
+    sentences.map((s,i) => ({...s, cluster:clusters[i]}))
+
+    const topics = centroids.map(({ centroid }, i) => {
+        const filtered = sentences.filter(({ cluster }) => cluster === i)
+        const similarities = filtered.map((s, i) => ({ ...s, similarity:getSimilarity(s.embeddings, centroid)}))
+        const sorted = [...similarities].sort(({ similarity: a }, { similarity: b }) => a > b ? 1 : -1)
+        const topSentences = sorted.filter((_, i) => i < 5)
+        return topSentences.map(({ text }) => text)
+    })
+
+    return topics
+}
+
+
+
 const getNotes = ():string[] => []
 const getConclusion = ():iConclusion => ({
     beginning:[],
@@ -40,14 +71,23 @@ const getConclusion = ():iConclusion => ({
 })
 
 
-const summarize = (sentences:iSentence[]):iSummary => {
+const summarize = async(transcript:string):Promise<iSummary> => {
+    const words = transcript.split(/[\s\n]+/)
+    const tokens = transcript.split('\n')
+    const embeddings = await getEmbeddings(tokens)
+    const sentences:iSentence[] = tokens.map((text, order) => ({
+        text,
+        order,
+        embeddings:embeddings[order]
+    }))
 
+    const dictionary = getDictionary(words)
     return {
-        titles:getTitles(sentences, []),
-        topics:getTopics(),
+        titles:getTitles(sentences),
+        topics:getTopics(sentences),
         notes:getNotes(),
         conclusions:getConclusion()
     }
 }
 
-summarize([])
+summarize('')
