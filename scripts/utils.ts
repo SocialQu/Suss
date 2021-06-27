d696438f189fd76effc48100c4412fcb69cec1f0
Santiago8888 <santiago.marti67@gmail.com>
2021-06-27 12:16:26 -0500

    Set dictionary methods as utils.

--- scripts/utils.ts
+++ scripts/utils.ts
@@ -1,3 +1,9 @@
+export interface iDictionary {[key:string]:number}
+export const getDictionary = (words:string[]) => words.reduce((d, i) => ({...d, [i]: d[i] ? d[i]+1 : 1}), {} as iDictionary)
+
+interface iGetInformation { words:string[], dictionary:iDictionary, tokens:number}
+export const getInformation = ({words, dictionary, tokens}:iGetInformation) => words.reduce((d, w) => d-=Math.log(dictionary[w]/tokens), 0)
+
 
 export const getSimilarity = (center:number[], embedding: number[]) => {
     if (center.length !== embedding.length) return Infinity
